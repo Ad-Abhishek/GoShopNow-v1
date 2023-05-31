cbf6f921821649f861e64e5f5c29a2fdb35668e2
Abhishek Adhikari <abhishek.adhikari276@gmail.com>
2023-05-31 23:20:14 +0545

    add Message component

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,6 +1,7 @@
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product';
 import Loader from '../components/Loader';
+import Message from '../components/Message';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 const HomeScreen = (product) => {
@@ -11,7 +12,9 @@ const HomeScreen = (product) => {
     { isLoading ? (
       <Loader />
     ) : error ? (
-      <div>{ error?.data?.message || error.error }</div>
+      <Message variant='danger'>
+        { error?.data?.message || error.error }
+      </Message>  
     ) : (
       <>
         <h1>Latest Products</h1>
