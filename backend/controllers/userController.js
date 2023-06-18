6ba9484520c565d1514672e89b7e95d5786cf56f
Abhishek Adhikari <abhishek.adhikari276@gmail.com>
2023-06-18 16:19:30 +0545

    update userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -41,7 +41,7 @@ const registerUser = asyncHandler(async (req, res) => {
     const user = await User.create({
         name,
         email,
-        password
+        password,
     });
 
     if (user) {
