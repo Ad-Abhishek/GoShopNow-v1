8ca2bc9a8f1b8dca2a5a792ffb169633db9c9d96
Abhishek Adhikari <abhishek.adhikari276@gmail.com>
2023-06-21 03:35:08 +0545

    Update login screen

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -12,10 +12,34 @@ const LoginScreen = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const submitHandler = (e) => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [login, { isLoading }] = useLoginMutation();
+
+    const { userInfo } = useSelector((state) => state.auth);
+
+    const { search } = useLocation();
+    const sp = new URLSearchParams(search);
+    const redirect = sp.get('redirect') || '/';
+
+    useEffect(() => {
+        if (userInfo) {
+        navigate(redirect);
+        }
+    }, [navigate, redirect, userInfo]);
+
+
+    const submitHandler = async (e) => {
         e.preventDefault();
-        console.log('submit');
-    }
+        try {
+        const res = await login({ email, password }).unwrap();
+        dispatch(setCredentials({ ...res }));
+        navigate(redirect);
+        } catch (err) {
+        toast.error(err?.data?.message || err.error);
+        }
+    };
 
     return (
         <FormContainer>
